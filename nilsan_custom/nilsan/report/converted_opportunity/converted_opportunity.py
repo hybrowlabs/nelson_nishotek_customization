cfb9ea3907ccf40d2ec489f09ebe661db503aac7
Pragati Dike <pragati@mail.hybrowlabs.com>
2025-02-20 16:13:27 +0530

    |nilsan_custom|Converted Report|

--- nilsan_custom/nilsan/report/converted_opportunity/converted_opportunity.py
+++ nilsan_custom/nilsan/report/converted_opportunity/converted_opportunity.py
@@ -1,8 +1,5 @@
 # Copyright (c) 2025, Hybrowlabs and contributors
 # For license information, please see license.txt
-
-# import frappe
-
 import frappe
 from frappe.utils import formatdate
 
@@ -18,7 +15,7 @@ def execute(filters=None):
 def get_columns_for_converted_opportunity():
     return [
         {"label": "Month", "fieldname": "month", "fieldtype": "Data", "width": 120},
-        {"label": "Owner", "fieldname": "owner", "fieldtype": "Data", "width": 150},
+        {"label": "Sales Person", "fieldname": "owner", "fieldtype": "Data", "width": 150},
         {"label": "Balance Enquiry", "fieldname": "balance_enquiry", "fieldtype": "Currency", "width": 150},
         {"label": "Outgoing", "fieldname": "outgoing", "fieldtype": "Currency", "width": 150},
         {"label": "Grand Total", "fieldname": "grand_total", "fieldtype": "Currency", "width": 150}
@@ -26,12 +23,14 @@ def get_columns_for_converted_opportunity():
 
 def get_converted_opportunity_data():
     converted_data = frappe.db.sql("""
-        SELECT DATE_FORMAT(o.creation, '%M') AS month, o.owner,
+        SELECT DATE_FORMAT(o.creation, '%M') AS month, 
+               o.custom_sales_person AS owner,
                SUM(o.total) AS balance_enquiry,
-               COALESCE(SUM((SELECT SUM(q.base_total) FROM `tabQuotation` q WHERE q.opportunity = o.name)), 0) AS outgoing,
-               SUM(o.total) + COALESCE(SUM((SELECT SUM(q.base_total) FROM `tabQuotation` q WHERE q.opportunity = o.name)), 0) AS grand_total
+               COALESCE((SELECT SUM(q.base_total) FROM `tabQuotation` q WHERE q.opportunity = o.name AND q.docstatus != 2), 0) AS outgoing,
+               SUM(o.total) + COALESCE((SELECT SUM(q.base_total) FROM `tabQuotation` q WHERE q.opportunity = o.name AND q.docstatus != 2), 0) AS grand_total
         FROM `tabOpportunity` o
-        WHERE o.status = 'Converted'
+        WHERE o.status = 'Converted' 
+        AND o.custom_sales_person IS NOT NULL
         GROUP BY month, owner
     """, as_dict=True)
     
