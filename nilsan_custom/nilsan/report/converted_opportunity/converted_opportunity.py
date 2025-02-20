66c7c69fe5220886877ce29cef8e3db1a77c9bdb
Pragati Dike <pragati@mail.hybrowlabs.com>
2025-02-20 16:16:32 +0530

    |nilsan_custom|Converted Report|

--- nilsan_custom/nilsan/report/converted_opportunity/converted_opportunity.py
+++ nilsan_custom/nilsan/report/converted_opportunity/converted_opportunity.py
@@ -26,9 +26,10 @@ def get_converted_opportunity_data():
         SELECT DATE_FORMAT(o.creation, '%M') AS month, 
                o.custom_sales_person AS owner,
                SUM(o.total) AS balance_enquiry,
-               COALESCE((SELECT SUM(q.base_total) FROM `tabQuotation` q WHERE q.opportunity = o.name AND q.docstatus != 2), 0) AS outgoing,
-               SUM(o.total) + COALESCE((SELECT SUM(q.base_total) FROM `tabQuotation` q WHERE q.opportunity = o.name AND q.docstatus != 2), 0) AS grand_total
+               COALESCE(SUM(q.base_total), 0) AS outgoing,
+               SUM(o.total) + COALESCE(SUM(q.base_total), 0) AS grand_total
         FROM `tabOpportunity` o
+        LEFT JOIN `tabQuotation` q ON q.opportunity = o.name AND q.docstatus != 2
         WHERE o.status = 'Converted' 
         AND o.custom_sales_person IS NOT NULL
         GROUP BY month, owner
